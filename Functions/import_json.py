6838d872afdd6bb7e61b077c4d9c1fdf7e4d8ae8
Unknown <brandaoclement@hotmail.fr>
2018-02-19 10:36:57 +0100

    Import Json

---
+++ Functions/import_json.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 16 10:46:04 2018
+
+@author: brandao
+"""
+
+from os import listdir
+import json
+from tqdm import tqdm
+
+
+path = "/Users/brandao/Desktop/COURS/ProjetTableauDeBord/ProjetTableauDeBord2018/Json"
+
+def import_articles(path_source):
+    """
+        Summary:
+            Import a panel of article from robot group (g4) according to the
+            server structure :
+                [date] / path_source / newspaper / article
+            where "date" corresponds to the most recent repository.
+        In:
+            - path_source : a string which corresponds to the localisation
+                 of robot group (g4)
+        Out:
+            - articles : a dict of articles
+    """
+    # Initialisation
+    article = {}
+
+    sources = listdir(path_source)
+    # Loop: For each inewspaper
+    for js in sources:
+        # management of hidden repositories: required on macOS (.ds_store)
+        if not js.startswith('.'):
+            xdirpaper = path_source + '/' + js
+            # progress bar for each newspaper repository
+            with tqdm(desc=str(sources), total=len(xdirpaper)) as fbar:
+                with open(xdirpaper, 'r',
+                          encoding='utf-8') as dict_robot:
+                    article[js] = json.load(dict_robot)
+                fbar.update()
+                continue
+            # End newspaper repository
+        continue
+    # End all newspapers
+    return article
+
+articles = import_articles(path)
+
