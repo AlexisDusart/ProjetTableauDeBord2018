cadc853c6f37af0afbddc1fe126bd5a735ffc8b3
Unknown <brandaoclement@hotmail.fr>
2018-03-08 11:32:52 +0100

    Nouveau format entity

--- main.py
+++ main.py
@@ -17,15 +17,25 @@ import operator
 path = "/Users/brandao/Desktop/COURS/ProjetTableauDeBord/ProjetTableauDeBord2018/Json"
 
 articles = import_articles(path)
-tab_ent = []
+dic_sources = {}
 for sources in articles:
-    with tqdm(desc='JSONing', total=len(articles)) as pbar:
-    for cle_art in articles[sources]:
+    dic_sources[sources] = {}
+    with tqdm(desc='JSONing', total=len(articles[sources])) as pbar:
+        for cle_art in articles[sources]:
             content = cleanhtml(articles[sources][cle_art]['Contenu'])
             title = cleanhtml(articles[sources][cle_art]['Titre'])
+            date = articles[sources][cle_art]['Date']
             content_tok = tokeniz(content)
-            entity = handing_entity(sources, title, content_tok, tab_ent)
+            dic_sources[sources][title] = handing_entity(content_tok)
+#            entity = handing_entity(sources, title, date, content_tok, tab_ent)
             pbar.update()
+
+dicdic = {}
+dicdic['blabla'] = {}
+dicdic['blabla']['roro'] = 4
+dicdic['blabla']['rara'] = 4
+
+
 
 ent_text = []
 i=0
@@ -37,3 +47,6 @@ for valeur in ent_text:
     compte[valeur] += 1
     
 sorted_compte = sorted(compte.items(), key=operator.itemgetter(1), reverse = True)
+
+with open('/Users/brandao/Desktop/COURS/ProjetTableauDeBord/ProjetTableauDeBord2018/entity.json', 'w') as f:
+     json.dump(entity, f)
